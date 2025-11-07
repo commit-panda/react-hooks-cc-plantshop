e340feb0d39db68a01d1646dc0b2a4fccc2ee872
commit-panda <sjmanyarkiy@gmail.com>
2025-11-07 22:27:48 +0300

    passed addition handler to NewPlantForm on PlantPage.js

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -4,13 +4,13 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 import { useState } from 'react'
 
-function PlantPage({plants}) {
+function PlantPage({plants, onAddPlant}) {
   
   const [searchItem, setSearchItem] = useState('');
 
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={onAddPlant}/>
       <Search searchItem = {searchItem} onSearchChange={setSearchItem}/>
       <PlantList plants = {plants} searchItem = {searchItem}/>
     </main>
